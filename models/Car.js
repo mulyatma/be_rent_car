30eac73d042703ff662174a9da79a2b233544cfc
Mulya Tri Atmaja <mulyaatmaja604@gmail.com>
2024-06-27 21:06:42 +0700

    add name car at carSchema

--- models/Car.js
+++ models/Car.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const carSchema = new mongoose.Schema({
+    nameCar: { type: String, required: true },
     img: { type: String, required: true },
     passenger: { type: Number, required: true },
     oil: { type: String, required: true },
